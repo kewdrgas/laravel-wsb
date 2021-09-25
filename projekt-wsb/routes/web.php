dafcf0735344960c39233bdb9ff0880a0341c3fe
KewDrgas <kewin.drgas@gmail.com>
2021-09-25 17:40:05 +0200

    Koniec-zajec

--- projekt-wsb/routes/web.php
+++ projekt-wsb/routes/web.php
@@ -16,3 +16,48 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('wsb',function (){
+   // return view('wsb');
+/*
+    return ['name' => 'Kewin',
+    'surname' => 'Drgas',
+     'city' => 'Rome'
+];
+*/
+return view('wsb',[
+    'name'     => 'Kewin',
+    'surname'  => 'Drgas',
+    'city'     => 'Rome'
+]);
+});
+Route::get('/pages/{data}',function($data){
+    $site =[
+        'kontakt'              => 'jan@o2.pl',
+        'informacje o stronie' => 'blabla',
+        'strona domowa'        => 'blabla'
+    ]; 
+
+    return $site[$data];
+});
+
+Route::get('/address/{city?}/{street?}/{zipcode?}',function(String $city='-', String $street='-', int $zipcode=null){
+    $zipcode = substr($zipcode,0,2)."-".substr($zipcode,2,3);
+    echo <<< ADDRESS
+     Miasto  $city <br>
+     Ulica: $street <br>
+     Kod pocztowy: $zipcode
+     <hr>
+ADDRESS;
+})->name('address');
+
+Route::redirect('adres/{city?}/{street?}/{zipcode?}','/address/{city?}/{street?}/{zipcode?}');
+Route::prefix('admin')->group(function(){
+    Route::get('/home/{name}', function (String $name){
+        echo "Witaj $name na stronie admina";
+    })->where(['name'=>'[A-Za-z]+']);
+
+    Route::get('/users', function (){
+        echo "Użytkownicy serwisu";
+    });
+});
+Route::redirect('admin/{name}','/admin/home/{name}');
